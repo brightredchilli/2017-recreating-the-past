fa602e07961a9c0617a2b2404cded79a5c2b6fd2
Ying Quan Tan <ying.quan.tan@gmail.com>
2017-10-14 12:41:15 -0400

    finished maeda poster

--- murielcooper2/src/ofApp.cpp
+++ murielcooper2/src/ofApp.cpp
@@ -5,8 +5,7 @@ void ofApp::setup(){
     ofBackground(0);
 //    ofSetBackgroundAuto(false);
     center.set(ofGetWidth(), ofGetHeight());
-    font.load("avenirnext.ttc", 80, true, false, true, 0.000001);
-//    font.setLetterSpacing(0.94);
+    font.load("AkzidenzGrotesk-BoldExtended.otf", 160, true, false, true, 0.000001);
 }
 
 //--------------------------------------------------------------
@@ -17,95 +16,36 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 
-//    if (ofGetFrameNum() == 6) {
-//        ofPoint position = ofPoint(center.x, 0);
-//        int fontSizeStep = 5;
-//
-//        float t = 1;
-//        int count = 0;
-//        while(position.y < ofGetHeight()) {
-//
-//            if (t < 0) {
-//                t = 0;
-//            }
-//            //            float out = powf(t, 5);
-////            float out = function_CubicBezier(t, 0.433, 0.05, 0.81, 0.057);
-//            float out = function_CubicBezier(t, 0.513, 0.152, 0.917, 0.3);
-////            float out = function_VariableStaircase(t, 1., 4);
-//
-//            int size = ofMap(out, 0, 1, 3, 3);
-//            font.load("SF.otf", size, true, true, true);
-//            font.setLetterSpacing(0.94);
-//            auto bounds = font.getStringBoundingBox("aaaa", 0, 0);
-//
-//            float adjustedHeight = bounds.height * 0.98;
-//            font.drawString("aaaa", position.x - bounds.width/2, position.y + adjustedHeight);
-//
-////            float testWidth = ofMap(size, 5, 180, 0, ofGetWidth()/2);
-////            ofDrawRectangle(center.x - testWidth/2, count * 20, testWidth , 20);
-//            position.y += adjustedHeight;
-//            t -= 0.065;
-//
-//
-//            count++;
-//        }
-//    }
-
     ofPushMatrix();
     ofScale(0.5, 0.5);
 
-    string myString = "technologic";
+    string myString = "TECHNO";
     auto bounds = font.getStringBoundingBox(myString, 0, 0);
     bounds.x = center.x - bounds.width/2;
-
-
-//    font.drawString(myString, bounds.x - 4, bounds.getBottom());
-//    ofPushStyle();
-//    ofSetColor(255, 100, 0, 100);
-//    ofDrawRectangle(bounds);
-//    ofPopStyle();
-
 
     auto previousBounds = bounds;
     float t = 1;
     int count = 0;
+    float scale = 0.7408;//ofMap(mouseX, 0, ofGetWidth()*2, 0.8, 1.0);
+    float start = 0.0434; //ofMap(mouseY, 0, ofGetHeight()*2, 0.04, 0.1);
     while(previousBounds.y < ofGetHeight() * 2) {
-
-
-//        float out = function_CubicBezier(t, 0.513, 0.152, 0.917, 0.3);
-//        float out = function_CubicBezier(t, 0.433, 0.05, 0.81, 0.057);
-        float power = ofMap(mouseX, 0, ofGetWidth() * 2, 0, 10);
-        float out = ofClamp(powf(t, 7) + 0.08, 0.08, 1);
-
-//        ofPushStyle();
-//        ofSetColor(0, 255, 0, 120);
-//        float testWidth = ofMap(out, 0, 1, 0, ofGetWidth()*2);
-//        ofDrawRectangle(0, count * 30, testWidth , 30);
-//        ofPopStyle();
-
+        float out = t;
         auto newBounds = ofRectangle(bounds);
         newBounds.scaleFromCenter(out);
         newBounds.y = previousBounds.y + previousBounds.height;
-//        ofPushStyle();
-//        ofSetColor(0, 255, 0, 120);
-//        ofDrawRectangle(newBounds);
-//        ofPopStyle();
 
         ofPushMatrix();
         ofTranslate(newBounds.getTopLeft());
         ofScale(out, out);
-//        float adjustedHeight = adjustedBounds.height * 0.98;
         font.drawString(myString, 0, newBounds.height * 1/out);
-
         ofPopMatrix();
 
-        //            float testWidth = ofMap(size, 5, 180, 0, ofGetWidth()/2);
-        //            ofDrawRectangle(center.x - testWidth/2, count * 20, testWidth , 20);
-//        position.y += adjustedHeight;
         previousBounds = newBounds;
-        t -= 0.03;
-
-
+        t *= scale;
+        scale += start * (t);
+        if (scale >= 1) {
+            scale = 1;
+        }
         count++;
     }
 
