8ff50d90ee9cba8ea0d1153ed9d02242c8fc6e44
Ying Quan Tan <ying.quan.tan@gmail.com>
2017-11-29 21:54:33 -0500

    made changes to animate john maeda sketch

--- murielcooper2/src/ofApp.cpp
+++ murielcooper2/src/ofApp.cpp
@@ -1,11 +1,13 @@
 #include "ofApp.h"
 #include "EasingFunctions.hpp"
+
+
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofBackground(0);
 //    ofSetBackgroundAuto(false);
     font.load("AkzidenzGrotesk-BoldExtended.otf", 100, true, false, true, 0.000001);
-    myString = "TECHNO";
+    myString = "ROBOT";
     vector<ofTTFCharacter> characters = font.getStringAsPoints(myString);
     meshOutlines.clear();
     for (ofTTFCharacter c : characters) {
@@ -15,40 +17,53 @@ void ofApp::setup(){
     textToScreenWidthRatio = ofGetWidth()/bounds.width;
     center.set(ofGetWidth()/2, ofGetHeight()/2);
     center /= textToScreenWidthRatio;
+
+    csv.load(myString + "-scalepoints.csv");
+    for (ofxCsvRow row : csv) {
+        ScalePoint point(row.getFloat(0), row.getFloat(1), row.getFloat(2));
+        csvScalePoints.push_back(point);
+    }
+
+    initialScale.set("scale", 0.7408, 0.718, 0.9);
+    initialScaleBy.set("scale by", 0.0434, 0.04, 0.1);
+
+    panel.setup();
+    panel.add(initialScale);
+    panel.add(initialScaleBy);
+    panel.loadFromFile("settings.xml");
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    initialScale = 0.7408; //ofMap(mouseX, 0, ofGetWidth()*2, 0.8, 1.0); //0.7408;
-    initialScaleBy = 0.0434; //ofMap(mouseY, 0, ofGetHeight()*2, 0.04, 0.1); //0.0434;
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
-    float time = fmod(ofGetElapsedTimef() * 0.5, HALF_PI);
-    float s = 1 - (ofGetElapsedTimef() * 0.01); // sin(time * 0.5);
     ofPushMatrix();
-    ofTranslate(ofGetWidth()/2, ofGetHeight());
-    ofScale(s, s);
-    ofTranslate(-ofGetWidth()/2, -ofGetHeight() * s);
-    drawDownwards();
-  //  drawUpwards();
+    if (csvScalePoints.size() > 0) {
+        drawTexts(csvScalePoints);
+    } else {
+        drawDownwards();
+    }
+
     ofPopMatrix();
     if (recording) {
         ofImage img = ofImage();
         img.grabScreen(0, 0 , ofGetWidth(), ofGetHeight());
         img.save("screenshot " + ofGetTimestampString() + ".png");
     }
+
+//    panel.draw();
 }
 
 void ofApp::drawDownwards() {
+    float startTime = ofGetElapsedTimef();
     ofPushMatrix();
     ofScale(textToScreenWidthRatio, textToScreenWidthRatio);
+    recorder.clear();
 
     auto bounds = font.getStringBoundingBox(myString, 0, 0);
-    bounds.x = center.x - bounds.width/2;
-    bounds.y = 0;
+    bounds.x = center.x - bounds.width/2 - bounds.x;
     auto previousBounds = bounds;
     previousBounds.y -= bounds.height; // some accounting so that the bounds is correct on first run
 
@@ -63,10 +78,10 @@ void ofApp::drawDownwards() {
         newBounds.y = previousBounds.y + previousBounds.height;
 
         // debug rectangle
-        //        ofPushStyle();
-        //        ofSetColor(200, count*20, count*20, 100);
-        //        ofDrawRectangle(newBounds.x, newBounds.y, newBounds.width, newBounds.height);
-        //        ofPopStyle();
+        ofPushStyle();
+        ofSetColor(200, count*20, count*20, 100);
+        ofDrawRectangle(newBounds.x, newBounds.y, newBounds.width, newBounds.height);
+        ofPopStyle();
 
         ofPushMatrix();
         ofTranslate(newBounds.getBottomLeft());
@@ -74,6 +89,11 @@ void ofApp::drawDownwards() {
         for (ofMesh mesh : meshOutlines) {
             mesh.draw();
         }
+        ofxCsvRow row;
+        row.addFloat(newBounds.getBottomLeft().x);
+        row.addFloat(newBounds.getBottomLeft().y);
+        row.addFloat(out);
+        recorder.addRow(row);
 
         ofPopMatrix();
 
@@ -84,13 +104,42 @@ void ofApp::drawDownwards() {
             scale = 1;
         }
         count++;
+
+        // do a gut check, if we have spent too much time in this frame, bail.
+
+        if (ofGetElapsedTimef() - startTime > 1.0) {
+            break;
+        }
     }
 
     ofPopMatrix();
+    recorder.save(myString + "-scalepoints.csv");
 }
 
-void ofApp::drawMeshOutlines() {
+void ofApp::drawTexts(vector<ScalePoint> scalePoints) {
+    ofPushMatrix();
+    ofScale(textToScreenWidthRatio, textToScreenWidthRatio);
 
+    float duration = 2;
+    float time = fmod(ofGetElapsedTimef(), duration);
+    float pct = ofMap(time, 0, duration, 0, 1);
+
+    for (int i = 0; i < scalePoints.size() - 1; i++) {
+        ScalePoint current = scalePoints[i];
+        ScalePoint next = scalePoints[i+1];
+
+        float xLerped = ofMap(pct, 0, 1, current.x, next.x);
+        float yLerped = ofMap(pct, 0, 1, current.y, next.y);
+        float scaleLerped = ofMap(pct, 0, 1, current.scale, next.scale);
+        ofPushMatrix();
+        ofTranslate(xLerped, yLerped);
+        ofScale(scaleLerped, scaleLerped);
+        for (ofMesh mesh : meshOutlines) {
+            mesh.draw();
+        }
+        ofPopMatrix();
+    }
+    ofPopMatrix();
 }
 
 
@@ -98,6 +147,10 @@ void ofApp::drawMeshOutlines() {
 void ofApp::keyPressed(int key){
     if (key == 'x') {
         recording = !recording;
+    }
+
+    if (key == 's') {
+        panel.saveToFile("settings.xml");
     }
 }
 
@@ -108,5 +161,6 @@ void ofApp::keyReleased(int key){
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    initialScale = ofMap(mouseX, 0, ofGetWidth(), initialScale.getMin(), initialScale.getMax());
+    initialScaleBy = ofMap(mouseY, 0, ofGetHeight(), initialScaleBy.getMin(), initialScaleBy.getMax());
 }
--- murielcooper2/src/ofApp.h
+++ murielcooper2/src/ofApp.h
@@ -1,6 +1,17 @@
 #pragma once
 
 #include "ofMain.h"
+#import "ofxCsv.h"
+#import "ofxGui.h"
+
+struct ScalePoint {
+    float x, y; // bottom left coordinates
+    float scale;
+
+    ScalePoint(float x, float y, float scale) : x(x), y(y), scale(scale)
+    {
+    }
+};
 
 class ofApp : public ofBaseApp{
 
@@ -13,16 +24,20 @@ class ofApp : public ofBaseApp{
 		void keyReleased(int key);
 		void mouseMoved(int x, int y );
 
+    ofxCsv csv;
+    ofxCsv recorder;
     ofPoint center;
     ofTrueTypeFont font;
     vector<ofMesh> meshOutlines;
+    vector<ScalePoint> csvScalePoints;
     string myString;
-    float initialScale;
-    float initialScaleBy;
+    ofxPanel panel;
+    ofParameter < float > initialScale;
+    ofParameter < float >initialScaleBy;
     float textToScreenWidthRatio;
     float textBounds;
 
     bool recording;
-    void drawMeshOutlines();
     void drawDownwards();
+    void drawTexts(vector<ScalePoint> scalePoints);
 };
